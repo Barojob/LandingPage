7dfae7efe120464e6e85f07f08738163c7461866
Yoo-yongseok <hpsyong@gmail.com>
2026-06-05 15:37:37 +0900

    feat : 이동 대기 스피어 추가

--- src/components/Download.tsx
+++ src/components/Download.tsx
@@ -1,21 +1,39 @@
 import { useEffect } from "react";
+import { APP_STORE_URL, PLAY_STORE_URL } from "../utils/constants";
+import "../pages/landing.css";
 
 const Download = () => {
   useEffect(() => {
     const userAgent = navigator.userAgent.toLowerCase();
 
     if (/android/.test(userAgent)) {
-      window.location.href =
-        "https://play.google.com/store/apps/details?id=com.barojob.app&hl=ko";
+      window.location.href = PLAY_STORE_URL;
     } else if (/iphone|ipad|ipod/.test(userAgent)) {
-      window.location.href =
-        "https://apps.apple.com/kr/app/%EC%9D%B8%EB%A0%A5%ED%8A%B9%EA%B3%B5%EB%8C%80/id6766862592";
+      window.location.href = APP_STORE_URL;
     } else {
       window.location.href = "/";
     }
   }, []);
 
-  return <div>스토어로 이동 중입니다</div>;
+  return (
+    <div className="download-page">
+      <div className="download-page__card">
+        <div className="download-page__brand">
+          <img
+            src="/images/Logo.png"
+            alt=""
+            width={42}
+            height={42}
+            draggable={false}
+          />
+          인력특공대
+        </div>
+        <div className="download-page__spinner" aria-hidden="true" />
+        <p className="download-page__title">스토어로 이동 중입니다</p>
+        <p className="download-page__desc">잠시만 기다려 주세요</p>
+      </div>
+    </div>
+  );
 };
 
 export default Download;
